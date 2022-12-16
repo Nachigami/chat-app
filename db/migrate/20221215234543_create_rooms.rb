795aeaa82ca0ac74a370cae656e79e544dce2b4c
Nachigami <satoshinachigami@gmail.com>
2022-12-16 15:34:38 +0900

    新規作成画面の表示

--- db/migrate/20221215234543_create_rooms.rb
+++ db/migrate/20221215234543_create_rooms.rb
@@ -1,8 +1,10 @@
-class CreateRooms < ActiveRecord::Migration[6.0]
+class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
-    create_table :rooms do |t|
-      t.string :name, null: false
-      t.timestamps
+    create_table :users do |t|
+      ## Database authenticatable
+      t.string :name,               null: false
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
     end
   end
 end
